03b70407dfb099a0a9019e2f67964e9dcaccba06
George Merida <gmerida92@gmail.com>
2023-03-05 12:13:09 -0800

    Able to view images

--- react-app/src/components/BusinessPage/MainComponent/TitleHeader/ImageBackgroundList/ImageBackgroundList.js
+++ react-app/src/components/BusinessPage/MainComponent/TitleHeader/ImageBackgroundList/ImageBackgroundList.js
@@ -10,25 +10,35 @@ import { useParams } from 'react-router-dom';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
+import { Modal } from '../../../../../context/Modal';
+
+import ImageCarouselModal from './ImageCarouselModal/ImageCarouselModal';
+
 function ImageBackgroundList({ id }) {
 
-    const images = useSelector((state) => state?.singleBusiness[id]?.Images)
-
-    return (
-        // <>Test</>
-        <ImageList sx={{ width: '100%', height: 300, overflow:'hidden'}} cols={3} rowHeight={164}>
-          {images?.map((item) => (
-            <ImageListItem key={item?.image_url}>
-              <img
-                src={`${item?.image_url}?w=164&h=164&fit=crop&auto=format`}
-                srcSet={`${item?.image_url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                alt={item?.tag}
-                loading="lazy"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-    );
+  const images = useSelector((state) => state?.singleBusiness[id]?.Images)
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      {(Array.isArray(images) && images.length > 0) && <ImageList onClick={() => setShowModal(true)} sx={{ width: '100%', height: 300, overflow: 'hidden', cursor: 'pointer' }} cols={3} rowHeight={164}>
+        {images?.map((item) => (
+          <ImageListItem key={item?.image_url}>
+            <img
+              src={`${item?.image_url}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${item?.image_url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={item?.tag}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>}
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <ImageCarouselModal images={images} />
+        </Modal>
+      )}
+    </>
+  );
 }
 
 
